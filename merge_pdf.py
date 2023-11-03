77f5809b11f84cf0e840a6c0b447f83d94121660
TowsifMuhtadiKhan <towsifanroidmarket@gmail.com>
2023-11-03 19:15:05 +0600

    merge_pdf added

--- merge_pdf.py
+++ merge_pdf.py
@@ -1,3 +1,5 @@
+# Before run this code please install 'pip install PyPDF2'
+
 from PyPDF2 import PdfMerger
 
 def merge_pdfs(input_pdfs, output_pdf):
@@ -10,7 +12,7 @@ def merge_pdfs(input_pdfs, output_pdf):
     merger.close()
 
 # Example usage
-input_pdfs = ['file1.pdf', 'file2.pdf']  # List of PDF files to merge, raplce name according to your name
+input_pdfs = ['file1.pdf', 'file2.pdf']  # List of PDF files to merge, replace name according to your file(s)name
 output_pdf = 'merged_output.pdf'  # Output file name, change this according to your requirement
 
 merge_pdfs(input_pdfs, output_pdf)
